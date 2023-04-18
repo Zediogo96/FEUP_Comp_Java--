5256e027c5391c1c8e549f0f0ae08e18abcd56da
Eduardo Silva <up202005283@up.pt>
2023-04-18 17:01:57 +0100

    Updated Project

---
+++ test/fixtures/public/cpf/4_jasmin/arithmetic/ComplexAdd.java
@@ -0,0 +1,10 @@
+class ComplexAdd {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a = 1;
+        b = 7;
+        c = a + b + 1 + 4 + 5 + 1;
+    }
+}
