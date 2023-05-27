315ffa77e78bf706b41043900d21eb0c0844c45c
up202003529 <up202003529@fe.up.pt>
2023-05-27 19:21:46 +0100

    more code cleanup

--- src/main/pt/up/fe/comp2023/ollir/optimizations/ConstPropVisitor.java
+++ src/main/pt/up/fe/comp2023/ollir/optimizations/ConstPropVisitor.java
@@ -22,6 +22,20 @@ public class ConstPropVisitor extends PreorderJmmVisitor<ConstPropParameters, Bo
         addVisit("Assignment", this::dealWithAssignment);
     }
 
+    private static void intersectMaps(Map<String, String> res, Map<String, String> map1, Map<String, String> map2) {
+        Map<String, String> mapFiltered = map2.entrySet().stream().filter(map -> {
+            if (map1.containsKey(map.getKey())) {
+                String val = map1.get(map.getKey());
+                return val.equals(map.getValue());
+            } else {
+                return false;
+            }
+        }).collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        res.clear();
+        res.putAll(mapFiltered);
+    }
+
     public Boolean defaultVisit(JmmNode node, ConstPropParameters constPropPar) {
         boolean changes = false;
 
@@ -67,9 +81,7 @@ public class ConstPropVisitor extends PreorderJmmVisitor<ConstPropParameters, Bo
         if (!constPropPar.toRemoveAssigned()) {
             JmmNode newNode = node.getChildren().get(0);
             switch (newNode.getKind()) {
-                case "Integer", "Boolean" -> {
-                    constPropPar.getConstants().put(assigneeName, newNode.get("value"));
-                }
+                case "Integer", "Boolean" -> constPropPar.getConstants().put(assigneeName, newNode.get("value"));
             }
         }
 
@@ -77,7 +89,7 @@ public class ConstPropVisitor extends PreorderJmmVisitor<ConstPropParameters, Bo
     }
 
     private Boolean dealWithWhile(JmmNode node, ConstPropParameters constPropPar) {
-        boolean changes = false;
+        boolean changes;
 
         JmmNode conditional_node = node.getJmmChild(0);
         JmmNode expression_node = node.getJmmChild(1);
@@ -86,9 +98,8 @@ public class ConstPropVisitor extends PreorderJmmVisitor<ConstPropParameters, Bo
             changes = visit(expression_node, constPropPar);
 
         } else {
-            ConstPropParameters constPropagationParamCopy = new ConstPropParameters(constPropPar);
+            ConstPropParameters constPropPar_Copy = new ConstPropParameters(constPropPar);
 
-            // 1st remove all that are assigned inside the scope
             constPropPar.setToRemoveAssigned(true);
             changes = visit(expression_node, constPropPar);
             constPropPar.setToRemoveAssigned(false);
@@ -96,28 +107,11 @@ public class ConstPropVisitor extends PreorderJmmVisitor<ConstPropParameters, Bo
             changes = visit(conditional_node, constPropPar) || changes;
             changes = visit(expression_node, constPropPar) || changes;
 
-            ConstPropVisitor.intersectMaps(
-                    constPropPar.getConstants(),
-                    constPropPar.getConstants(),
-                    constPropagationParamCopy.getConstants()
+            ConstPropVisitor.intersectMaps(constPropPar.getConstants(), constPropPar.getConstants(), constPropPar_Copy.getConstants()
             );
         }
 
         return changes;
-    }
-
-    private static void intersectMaps(Map<String, String> res, Map<String, String> map1, Map<String, String> map2) {
-        Map<String, String> mapFiltered = map2.entrySet().stream().filter(map -> {
-            if (map1.containsKey(map.getKey())) {
-                String val = map1.get(map.getKey());
-                return val.equals(map.getValue());
-            } else {
-                return false;
-            }
-        }).collect(java.util.stream.Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-        res.clear();
-        res.putAll(mapFiltered);
     }
 
     @Override
